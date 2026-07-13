f206f7a34cddd70d5dedc67c51b7fc884a066b74
stepik26 <max_aron@gmx.de>
2026-07-14 00:55:29 +0200

    attempt1

---
+++ lib/presentation/helpers.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+
+  const Button({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: OutlinedButton(
+        onPressed: () {},
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.grey[400],
+          foregroundColor: Colors.black,
+          side: const BorderSide(color: Colors.black),
+          shape: const RoundedRectangleBorder(),
+          padding: EdgeInsets.zero,
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class Snack {
+  final IconData icon;
+  final String price;
+
+  const Snack({required this.icon, required this.price});
+}
+
+class ProduktFach extends StatelessWidget {
+  final Snack snack;
+
+  const ProduktFach({super.key, required this.snack});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 2),
+      ),
+
+      child: Column(
+        children: [
+          Expanded(
+            child: Icon(snack.icon, size: 40, color: Colors.purple),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    border: Border(top: BorderSide(), right: BorderSide()),
+                  ),
+                  child: const Center(
+                    child: Text('3 St.', style: TextStyle(fontSize: 10)),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    border: Border(top: BorderSide()),
+                  ),
+                  child: const Center(
+                    child: Text('1,50€', style: TextStyle(fontSize: 10)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
